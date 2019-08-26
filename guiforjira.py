16ae304abd18f824c211e9f0ea8100d9d14ae248
Öner ELMAS <oelmas@gmail.com>
2019-08-26 13:42:36 +0300

    Kullaniciya ait issue ve baslıklar tablo olarak gösteriliyor

--- guiforjira.py
+++ guiforjira.py
@@ -2,16 +2,37 @@
 
 import sys
 
-from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.QtWidgets import QDialog, QApplication, QTableView
+from PyQt5.QtCore import QAbstractTableModel, Qt
 from login import *
 from jira import JIRA, JIRAError
-from time import strftime
+from datetime import datetime
+import pandas as pd
+import numpy as np
 
 
-def calculate_works(datetime1, datetime2):
-    arg1 = datetime1.strptime(datetime1,'%Y-%m-%dT%H:%M:%S.%f%z')
-    arg2 = datetime1.strptime(datetime2,'%Y-%m-%dT%H:%M:%S.%f%z')
-    result = arg2 - arg1
+class pandasModel(QAbstractTableModel):
+
+    def __init__(self, data):
+        QAbstractTableModel.__init__(self)
+        self._data = data
+
+    def rowCount(self, parent=None):
+        return self._data.shape[0]
+
+    def columnCount(self, parnet=None):
+        return self._data.shape[1]
+
+    def data(self, index, role=Qt.DisplayRole):
+        if index.isValid():
+            if role == Qt.DisplayRole:
+                return str(self._data.iloc[index.row(), index.column()])
+        return None
+
+    def headerData(self, col, orientation, role):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            return self._data.columns[col]
+        return None
 
 
 class LoginGui(QDialog):
@@ -47,8 +68,16 @@ class LoginGui(QDialog):
                 users = jiraMy.search_users('.')
                 self.ui.lvUsers.addItems([user.name for user in users])
 
+    def calculate_works(self, datetime1, datetime2):
+        arg1 = datetime.strptime(datetime1, '%Y-%m-%dT%H:%M:%S.%f%z')
+        arg2 = datetime.strptime(datetime2, '%Y-%m-%dT%H:%M:%S.%f%z')
+        result = arg2 - arg1
+        return result.days, result.mins, result.seconds
+
     def getUserIssues(self):
         global issues
+        allpersonal_issues = pd.DataFrame()
+        global model
         try:
             prj = self.ui.lvProjects.currentItem().text()
             print([prj])
@@ -64,13 +93,48 @@ class LoginGui(QDialog):
 
             block_size = 100
             block_num = 0
-            issues = jiraMy.search_issues(query_issues, startAt=block_num * block_size, maxResults=block_size,fields="issuetype, created, duedate, resolutiondate, reporter, assignee, status")
+            issues = jiraMy.search_issues(query_issues, startAt=block_num * block_size, maxResults=block_size,
+                                          fields="issuetype, created, duedate, resolutiondate, reporter, assignee, status")
         except JIRAError as je:
             print(je.status_code, je.text)
         finally:
+
             if len(issues) > 0:
 
-                print([calculate_works(issue.fields.created, issue.fields.duedate) for issue in issues])
+                for issue in issues:
+
+                    d = {
+                        'key': issue.key,
+                        'assignee': issue.fields.assignee,
+                        # 'creator': issue.fields.creator,
+                        'reporter': issue.fields.reporter,
+                        'created': issue.fields.created.split('T')[0],
+                        'duedate': issue.fields.duedate,
+                        # 'components': issue.fields.components,
+                        # 'description': issue.fields.description,
+                        # 'summary': issue.fields.summary,
+                        # 'fixVersions': issue.fields.fixVersions,
+                        'subtask': issue.fields.issuetype.subtask,
+                        'issuetype': issue.fields.issuetype.name,
+                        # 'priority': issue.fields.priority.name,
+                        # 'resolution': issue.fields.resolution,
+                        'resolution.date': issue.fields.resolutiondate,
+                        'status.name': issue.fields.status.name,
+                        'status.description': issue.fields.status.description,
+                        # 'updated': issue.fields.updated,
+                        # 'versions': issue.fields.versions,
+                        # 'watches': issue.fields.watches.watchCount,
+                        # 'storypoints': issue.fields.customfield_10142
+                    }
+
+                    allpersonal_issues = allpersonal_issues.append(d, ignore_index=True)
+
+                    allpersonal_issues['Planned Work'] = pd.to_datetime(allpersonal_issues['duedate']) - pd.to_datetime(
+                        allpersonal_issues['created'])
+                    allpersonal_issues['Planned Work'] = allpersonal_issues['Planned Work'] / np.timedelta64(1, 'D')
+
+                model = pandasModel(allpersonal_issues)
+                self.ui.tbvIssuesOfUser.setModel(model)
 
 
 if __name__ == "__main__":
